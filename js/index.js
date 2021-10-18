1dd0d982d633b424cfa8b13ba25a7ca35f8cf2e9
Editing hero <editinghero999@gmail.com>
2021-10-18 10:34:36 +0530

    1.5.6

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ window.onscroll = function(){
 let progressHeight = (window.pageYOffset / totalHeight *100);
 progress.style.height = progressHeight + "%"
 };
+
+//preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load" , function(){
+    loader.style.display = "none"
+})
